f41a6d7dbf516dc9347aa5ea5d51b18bf886e7b1
ym-project <ym-project@protonmail.com>
2020-12-18 20:13:14 +0800

    add source code

---
+++ index.mjs
@@ -0,0 +1,42 @@
+function* brute(chars) {
+	let alphabet
+
+	if (Array.isArray(chars)) {
+		alphabet = chars.join('')
+	} else if (typeof chars === 'string') {
+		alphabet = chars
+	} else {
+		throw new TypeError('Argument "chars" should be array or string!')
+	}
+
+	let index = 1
+	const alphabetLength = alphabet.length
+	const to = [...Array(alphabetLength + 1).keys()]
+		.slice(1)
+		.map(it => A(it, alphabetLength))
+		.reduce((prev, current) => prev + current)
+
+	while (index <= to) {
+		yield numberToChars(index)
+		index++
+	}
+
+	function numberToChars(number) {
+		let chars = ''
+
+		while (number > 0) {
+			let n = (number - 1) % alphabetLength
+			chars = alphabet[n] + chars
+
+			number = Math.floor((number - n) / alphabetLength)
+		}
+
+		return chars
+	}
+}
+
+function A(m, n) {
+	return Math.pow(n, m)
+}
+
+export default brute
